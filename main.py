9cb908ae43a692908f878c17221936f3528236f6
cakeoisi <166780171+cakeoisi@users.noreply.github.com>
2024-06-21 10:42:10 +0900

    kadai9A2

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-from fastapi.responses import HTMLResponse #インポート
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-### コードいろいろ... ###
+app = FastAPI()
 
 @app.get("/index")
 def index():
     html_content = """
     <html>
         <head>
-            <title>Some HTML in here</title>
+            <title>My Custom Page</title>
         </head>
         <body>
-            <h1>Look ma! HTML!</h1>
+            <h1>Welcome to My Custom Page!</h1>
+            <p>This is a paragraph with some <strong>bold</strong> text.</p>
+            <p>Here's a list:</p>
+            <ul>
+                <li>First item</li>
+                <li>Second item</li>
+                <li>Third item</li>
+            </ul>
         </body>
     </html>
     """
